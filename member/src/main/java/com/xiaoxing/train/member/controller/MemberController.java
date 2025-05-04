22574440ef5f3c35458354c4ea879b7f0e6395b6
xiaoxing <3144750541@qq.com>
2025-05-04 18:04:23 +0800

    新增会员注册功能

--- member/src/main/java/com/xiaoxing/train/member/controller/MemberController.java
+++ member/src/main/java/com/xiaoxing/train/member/controller/MemberController.java
@@ -1,8 +1,12 @@
 package com.xiaoxing.train.member.controller;
 
+import cn.hutool.core.util.IdUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.xiaoxing.train.member.domain.Member;
 import com.xiaoxing.train.member.service.MemberService;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,5 +18,31 @@ public class MemberController {
     @GetMapping("/count")
     public Long count(){
         return memberService.count();
+    }
+
+    /**
+     * 会员注册
+     * @param mobile
+     * @return 会员id
+     */
+    @GetMapping("/register/{mobile}")
+    public long register(@PathVariable String mobile){
+        //判断手机号是否已经注册过了
+        QueryWrapper<Member> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("mobile",mobile);
+        Member one = memberService.getOne(queryWrapper);
+        if (one!=null){
+            throw new RuntimeException("该账号已注册");
+        }
+        //未注册，进行注册
+        Member member = new Member();
+        long id = IdUtil.getSnowflakeNextId();
+        member.setId(id);
+        member.setMobile(mobile);
+        boolean saved = memberService.save(member);
+        if (!saved){
+            throw new RuntimeException("注册失败");
+        }
+        return member.getId();
     }
 }
